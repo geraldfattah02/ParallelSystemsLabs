b7e2fa196054c1b855eec83d2d82d38e01c055ac
Gerald Fattah <gfattah@tesla.com>
2025-10-23 20:30:15 -0500

    last_fixes

--- handin/utils/unionfind.go
+++ handin/utils/unionfind.go
@@ -1,8 +1,11 @@
 package utils
 
+import "sync"
+
 type UnionFind struct {
 	parent []int
 	rank   []int
+	mu     sync.Mutex
 }
 
 func NewUnionFind(n int) *UnionFind {
@@ -16,6 +19,8 @@ func NewUnionFind(n int) *UnionFind {
 
 func (uf *UnionFind) Find(x int) int {
 	// Iterative path compression
+	uf.mu.Lock()
+	defer uf.mu.Unlock()
 	root := x
 	for uf.parent[root] != root {
 		root = uf.parent[root]
@@ -29,7 +34,10 @@ func (uf *UnionFind) Find(x int) int {
 	return root
 }
 func (uf *UnionFind) Union(a, b int) {
-	ra, rb := uf.Find(a), uf.Find(b)
+	uf.mu.Lock()
+	defer uf.mu.Unlock()
+
+	ra, rb := uf.findNoLock(a), uf.findNoLock(b)
 	if ra == rb {
 		return
 	}
@@ -43,10 +51,26 @@ func (uf *UnionFind) Union(a, b int) {
 	}
 }
 
+func (uf *UnionFind) findNoLock(x int) int {
+	root := x
+	for uf.parent[root] != root {
+		root = uf.parent[root]
+	}
+	for x != root {
+		parent := uf.parent[x]
+		uf.parent[x] = root
+		x = parent
+	}
+	return root
+}
+
 func (uf *UnionFind) Groups() [][]int {
+	uf.mu.Lock()
+	defer uf.mu.Unlock()
+
 	rootToGroup := make(map[int][]int)
 	for i := range uf.parent {
-		root := uf.Find(i)
+		root := uf.findNoLock(i)
 		rootToGroup[root] = append(rootToGroup[root], i)
 	}
 	groups := make([][]int, 0, len(rootToGroup))
